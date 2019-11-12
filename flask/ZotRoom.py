22b6fc0f958b01777927de1a771b634c9a4fc2bd
Jigar Hira <jigarhira354@gmail.com>
2019-11-12 01:14:33 -0800

    added html

--- flask/ZotRoom.py
+++ flask/ZotRoom.py
@@ -1,10 +1,10 @@
-from flask import Flask, escape, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Home Page</h1>'
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
